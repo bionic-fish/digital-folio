25dd1194ea45a5fb7be7c2e111667ca99ec53359
Chris <c.d.harris.dedoc@gmail.com>
2019-08-15 03:22:29 +1000

    mobile responsive index

--- src/js/animate.js
+++ src/js/animate.js
@@ -2,7 +2,7 @@
 const bannerNav = document.querySelector('.animated-nav');
 const bannerLeft = document.querySelector('.animated-left');
 const bannerRight = document.querySelector('.animated-right');
-const bannerBtn = document.querySelector('.animated-btn');
+const bannerBottom = document.querySelector('.animated-bottom');
 const bannerTitle = document.querySelector('.animated-title');
 
 const tl = new TimelineMax();
@@ -47,4 +47,35 @@ tl.fromTo(
     0.9,
     { opacity: 0, x: 30 },
     { opacity: 1, x: 0 },
+),
+
+tl.fromTo(
+    bannerBottom,
+    0.9,
+    { opacity: 0, x: 30 },
+    { opacity: 1, x: 0 },
+    "-=0.9"
 )
+
+
+// installMediaQueryWatcher("(min-width: 600px)", function(matches) {
+//     if(matches) {
+        
+       
+    // } else {
+        // animate mobile properties
+//         tl.fromTo(
+//             bannerNav,
+//             2,
+//             { height: "0%" },
+//             { height: "100%", ease: Power2.easeInOut },
+//         )
+//         // animate mobile properties
+//     }
+// });
+
+// function installMediaQueryWatcher(mediaQuery, layoutChangedCallback) {
+//     var mql = window.matchMedia(mediaQuery);
+//     mql.addEventListener(function(e) { return layoutChangedCallback(e.matches); });
+//     layoutChangedCallback(mql.matches);
+// }
